a428e667fd9a01bfc6099980dffb1b622a9a1d49
Yonashiro Yuu <cloneko@gmail.com>
2014-04-18 17:23:02 +0900

    Fix typo

--- rpm.sh
+++ rpm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo '"package":{'
-RPMS=`rpm -qa --queryformat '"%{NAME}" : { "velease": "%{RELEASE}", "arch": "%{ARCH}" \},' | sed 's/,$//'` 
+RPMS=`rpm -qa --queryformat '"%{NAME}" : { "version": "%{RELEASE}", "arch": "%{ARCH}" \},' | sed 's/,$//'` 
 echo $RPMS
 echo '},'
 
