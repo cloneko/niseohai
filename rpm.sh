c9d73b2f2f3b9557cc7dd1bd57fb6fb157e03b0b
Yonashiro Yuu <cloneko@gmail.com>
2014-04-18 17:38:26 +0900

    Fix version/release

--- rpm.sh
+++ rpm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo '"package":{'
-RPMS=`rpm -qa --queryformat '"%{NAME}" : { "version": "%{RELEASE}", "arch": "%{ARCH}" \},' | sed 's/,$//'` 
+RPMS=`rpm -qa --queryformat '"%{NAME}" : {"release" : "%{RELEASE}" "version": "%{VERSION}", "arch": "%{ARCH}" \},' | sed 's/,$//'` 
 echo $RPMS
 echo '},'
 
